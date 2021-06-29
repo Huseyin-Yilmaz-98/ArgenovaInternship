79a5a68f5374b9cbf17fe0c48f2daf9c3601e048
Huseyin Yilmaz <ylmzhsyn98@gmail.com>
2021-06-29 23:57:04 +0300

    added task-12

---
+++ task-12/panel/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="w-100 d-flex justify-content-center mb-5" style="margin-top: 4em">
+        <a href="/panel/users/index.php" class="text-white fw-bold mx-4 fs-3">Users</a>
+    </div>
+</header>
